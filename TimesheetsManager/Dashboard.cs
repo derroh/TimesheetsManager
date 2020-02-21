7f198be709a868c768177ca535b341502df4a85a
derroh <abucheriwitness@gmail.com>
2020-02-21 10:13:24 +0300

    Setting up

---
+++ TimesheetsManager/Dashboard.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimesheetsManager
+{
+    public partial class Dashboard : DevExpress.XtraEditors.XtraForm
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+        }
+    }
+}
